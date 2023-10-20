55675d6f379b0112998affb2432794e1278888da
Leestalion <lemarrecthomas@gmail.com>
2023-10-21 01:47:33 +0200

    v2 upload

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 
 
 <template >
-  <div class="flex justify-center w-screen h-screen">
+  <div class="flex justify-center w-screen h-screen bg-[#22272E] text-white">
     <div class="flex flex-col justify-center ">
 
-      <div class="bg-gray-300 p-10 rounded shadow-xl" v-if="alreadyVoted">
+      <div class="bg-[#1C2128] p-10 rounded shadow-xl mb-10" v-if="alreadyVoted">
         <h1>Arrete de forcer t'as déjà voté</h1>
       </div>
 
 
 
-      <div class="fixed z-50 top-0 left-0 bg-gray-300 w-full h-full flex items-center justify-center" v-if="showVotedPopup">
+      <div class="fixed z-50 top-0 left-0 bg-[#22272E] w-full h-full flex items-center justify-center"
+        v-if="showVotedPopup">
         <div class="">
           <div class="align-middle">
-            <div class="w-4/5 m-auto p-2 bg-white rounded shadow-md transition-all opacity-100">
+            <div
+              class="w-4/5 m-auto p-2 bg-[#22272E] border-[#444C56] border-solid border-2 rounded shadow-md transition-all opacity-100">
 
 
               <div class="m-2">
@@ -21,7 +23,7 @@
               </div>
 
               <div class="flex">
-                <button class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                <button class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded"
                   @click="closeVotedPopup">
                   retour
                 </button>
@@ -32,26 +34,27 @@
       </div>
 
 
-      <div class="fixed z-50 top-0 left-0 bg-gray-300 w-full h-full flex items-center justify-center" v-if="showPopup">
+      <div class="fixed z-50 top-0 left-0 bg-[#22272E] w-full h-full flex items-center justify-center"
+        v-if="showPopup">
         <div class="">
           <div class="align-middle">
-            <div class="w-4/5 m-auto p-2 bg-white rounded shadow-md transition-all opacity-100">
+            <div class="w-4/5 m-auto p-2 border-[#444C56] border-solid border-2 rounded shadow-md transition-all opacity-100">
 
 
               <div class="m-2">
                 <form>
                   <label for="password">Mot de passe :</label>
-                  <input type="text" name="password" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                  <input type="text" name="password" class="mt-1 block w-full px-3 py-2 bg-[#444C56] border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" v-model="password">
                 </form>
               </div>
 
               <div class="flex">
-                <button class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                <button class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded"
                   @click="confirmReset">
                   OK
                 </button>
-                <button class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                <button class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded"
                   @click="closePopup">
                   retour
                 </button>
@@ -62,38 +65,65 @@
       </div>
 
 
+      <div v-if="showMyVotePage" class="flex justify-center flex-col">
+
+        <h2 class="font-bold text-xl mb-2">{{ confirmedDate }}</h2>
+
+        <p v-for="(name, vote) in myVotes">
+        <p class="inline-block w-20"> {{ vote }} </p>
+        <p class="inline"> {{ name }} </p>
+        </p>
+
+        <input v-model="selectedDate" type="date" class="mt-1 block w-full px-3 py-2 bg-[#444C56] border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 text-black" />
+
+        <button class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded" @click="confirmDate">
+          confirmer
+        </button>
+
+        <button class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded" @click="backFromVotes">
+          retour
+        </button>
+
+        <p v-if="showErrorNoData">T'as pas voté ce jour là</p>
+
+      </div>
+
+
       <div v-if="showResult" class="flex justify-center flex-col">
         <p v-for="(user) in resultsSorted">
         <p class="inline-block w-20"> {{ user[0] }} </p>
         <p class="inline"> {{ user[1] }} </p>
         </p>
         <button @click="resetVote"
-          class="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Réinitialiser le
+          class="mt-10 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded">Réinitialiser le
           vote</button>
         <button @click="backResult"
-          class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Retour</button>
+          class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded">Retour</button>
       </div>
 
-      <div class="bg-gray-300 p-10 rounded shadow-xl" v-if="step2 & !alreadyVoted & !showResult">
+      <div class="bg-[#22272E] p-5 rounded shadow-xl border-[#444C56] border-solid border-2"
+        v-if="step2 & !alreadyVoted & !showResult & !showMyVotePage">
         <p class="font-thin text-sm">Vote de {{ connectedUser }}</p>
         <h1 class="font-bold text-xl mb-2">CLASSEMENT DES PIMPIMS</h1>
         <form>
-          <div v-for="(user) in Object.keys(users)" class="text-gray-700 text-base">
-            <p class="pt-4 font-bold">{{ user }}</p>
+          <div v-for="(user) in Object.keys(users)" class="text-white text-base">
+            <p class="pt-2 font-bold">{{ user }}</p>
             <input class="m-1" v-for="(item) in items" type="radio" :id=item.name :value=item.number v-model=users[user]>
             Rang : <p class="font-bold inline">{{
               users[user] }}</p>
           </div>
         </form>
-        <button @click="sendVote" class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Envoyer
+        <button @click="sendVote"
+          class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded">Envoyer
           le vote</button>
         <button @click="back"
-          class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Retour</button>
+          class="m-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded">Retour</button>
       </div>
 
 
-      <div v-else-if="!showResult" class="flex justify-center flex-col">
-        <p>T'es qui ?</p>
+      <div v-else-if="!showResult & !showMyVotePage" class="flex justify-center flex-col">
+        <p class="my-2">T'es qui ?</p>
         <select name="connectedUser" id="select-user" v-model="connectedUser"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           <option value="">t'es qui wesh</option>
@@ -108,13 +138,14 @@
         </select>
 
         <button @click="confirm"
-          class="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Confirmer</button>
+          class="mt-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded">Voter</button>
 
-      </div>
+        <button @click="seeVotes" class="mt-2 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded">Voir
+          mes votes</button>
 
-      <div v-if="!step2 & !showResult">
-        <button @click="results" class="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Voir
+        <button @click="results" class="mt-10 bg-[#373E47] hover:bg-[#444C56] text-white font-bold py-2 px-4 rounded">Voir
           les résultats</button>
+
       </div>
 
     </div>
@@ -124,11 +155,20 @@
 <script>
 
 import { ref } from 'vue'
-import { collection, deleteDoc, getDocs, getDoc, updateDoc, doc } from "firebase/firestore";
+import { collection, getDocs, getDoc, updateDoc, doc } from "firebase/firestore";
 import db from './firebase/init'
 
 export default {
   setup() {
+
+    const startingDay = ref(20);
+    const startingMonth = ref(9);
+    const selectedDate = ref(formatDate(new Date()));
+    const confirmedDate = ref(formatDate(new Date()));
+
+    const month = ref(new Date().getMonth());
+    const day = ref(dateToincrement(new Date()));
+    const dayRank = ref("rank" + day.value);
 
     const resultsSorted = ref([])
     const connectedUser = ref("");
@@ -139,6 +179,8 @@ export default {
     const showResult = ref(false);
     const showPopup = ref(false);
     const showVotedPopup = ref(false);
+    const showMyVotePage = ref(false);
+    const showErrorNoData = ref(false);
 
     const dbUsers = ref([])
     const userResult = ref({})
@@ -174,63 +216,124 @@ export default {
       "Nicolas": nic
     })
 
+    const myVotes = ref({
+      "Theo": 0,
+      "Thomas": 0,
+      "Estevan": 0,
+      "Labous": 0,
+      "Vincent": 0,
+      "Paul": 0,
+      "Simon": 0,
+      "Nicolas": 0
+    })
+
+    function dateToincrement(date) {
+      return date.getDate() - startingDay.value + (month.value - startingMonth.value) * 31;
+    }
+
+    function formatDate(date) {
+      const offset = date.getTimezoneOffset()
+      date = new Date(date.getTime() - (offset*60*1000))
+      return date.toISOString().split('T')[0]
+    }
+
     function back() {
       connectedUser.value = "";
       step2.value = false;
     }
 
-    async function getUsers() {
-      const querySnapshot = await getDocs(collection(db, "users"));
-      querySnapshot.forEach((doc) => {
-        dbUsers.value.push(doc.id);
-      });
+    async function seeVotes() {
+
+      showErrorNoData.value = false;
+
+      if (connectedUser.value) {
+
+        const userRef = doc(db, "users", connectedUser.value);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists() & "vote" + day.value in userSnap.data()) {
+          showMyVotePage.value = true;
+
+          myVotes.value = userSnap.data()["vote" + day.value];
+
+        } else {
+          showErrorNoData.value = true;
+        }
+      }
     }
 
-    async function setUserRank(user) {
-      console.log(users.value[user])
-      const userRef = doc(db, "users", user);
-      await updateDoc(userRef, {
-        rank: users.value[user]
-      });
+    async function confirmDate() {
+
+      showErrorNoData.value = false;
+
+      if (connectedUser.value) {
+        if (selectedDate.value) {
+
+          const chosenDay = dateToincrement(new Date(selectedDate.value));
+
+          const userRef = doc(db, "users", connectedUser.value);
+          const userSnap = await getDoc(userRef);
+
+          if (userSnap.exists() & "vote" + chosenDay in userSnap.data()) {
+
+            confirmedDate.value = selectedDate.value;
+            showMyVotePage.value = true;
+
+            myVotes.value = userSnap.data()["vote" + chosenDay];
+
+
+          } else {
+            showErrorNoData.value = true;
+          }
+        }
+      }
     }
 
     async function sendVote() {
+
       for (let user in users.value) {
+
         const userRef = doc(db, "users", user);
         const userSnap = await getDoc(userRef);
-        if (userSnap.exists() & "rank" in userSnap.data()) {
-          console.log(userSnap.data().rank);
-          await updateDoc(userRef, {
-            rank: users.value[user] + userSnap.data().rank
-          })
+
+        if (userSnap.exists() & "rank" + day.value in userSnap.data()) {
+
+          const data = {};
+          data[dayRank.value] = users.value[user] + userSnap.data().rank
+
+          await updateDoc(userRef, data)
+
           showVotedPopup.value = true;
           step2.value = false;
+
         } else {
-          await updateDoc(userRef, {
-            rank: users.value[user]
-          })
+
+          const data = {};
+          data[dayRank.value] = users.value[user]
+
+          await updateDoc(userRef, data)
+
           showVotedPopup.value = true;
           step2.value = false;
         }
 
       }
 
-      const userRef = doc(db, "users", connectedUser.value)
-      await updateDoc(userRef, {
-        voted: true
-      })
+      const userRef = doc(db, "users", connectedUser.value);
+      const dataVote = {};
+      dataVote["vote" + day.value] = users.value;
+
+      await updateDoc(userRef, dataVote)
     }
 
     async function confirm() {
       alreadyVoted.value = false;
-      console.log(connectedUser.value)
       if (connectedUser.value) {
         const userRef = doc(db, "users", connectedUser.value);
         const userSnap = await getDoc(userRef);
 
         if (userSnap.exists() & "voted" in userSnap.data() & userSnap.data().voted == true) {
           alreadyVoted.value = true;
-          console.log("already voted");
         } else {
           step2.value = true;
         }
@@ -246,23 +349,28 @@ export default {
       if (password.value == "pimpim23") {
         for (let user in users.value) {
           const userRef = doc(db, "users", user);
-          await updateDoc(userRef, {
-            voted: false,
-            rank: 0
-          });
+
+          const data = {};
+          data["voted"] = false;
+          data["rank" + day.value] = 0;
+          data["vote" + day.value] = []
+
+          await updateDoc(userRef, data);
         }
-      } 
-      
+      }
+
       showPopup.value = false;
     }
 
     async function results() {
+      alreadyVoted.value = false;
       showResult.value = true;
       var count = 0;
       const querySnapshot = await getDocs(collection(db, "users"));
       querySnapshot.forEach((doc) => {
-        if ("rank" in doc.data()) {
-          userResult.value[doc.id] = doc.data().rank;
+
+        if ("rank" + day.value in doc.data()) {
+          userResult.value[doc.id] = doc.data()["rank" + day.value];
         }
 
         if ("voted" in doc.data() & doc.data().voted == true) {
@@ -270,7 +378,7 @@ export default {
         }
       });
 
-      if(count == 0 ) {
+      if (count == 0) {
         count += 1;
       }
 
@@ -298,12 +406,15 @@ export default {
       showVotedPopup.value = false;
     }
 
+    function backFromVotes() {
+      showMyVotePage.value = false;
+      showErrorNoData.value = false;
+    }
+
 
     return {
       users,
       items,
-      setUserRank,
-      getUsers,
       dbUsers,
       sendVote,
       connectedUser,
@@ -322,7 +433,15 @@ export default {
       confirmReset,
       password,
       showVotedPopup,
-      closeVotedPopup
+      closeVotedPopup,
+      seeVotes,
+      showMyVotePage,
+      backFromVotes,
+      myVotes,
+      selectedDate,
+      confirmedDate,
+      confirmDate,
+      showErrorNoData
     }
   }
 }
